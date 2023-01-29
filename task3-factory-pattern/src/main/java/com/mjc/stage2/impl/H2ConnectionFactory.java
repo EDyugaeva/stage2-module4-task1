2e13845af9cc3328307b0dbf65b2f5e4df4921dd
EDyugaeva <ed2408@yandex.ru>
2023-01-29 18:41:19 +0500

    commit

--- task3-factory-pattern/src/main/java/com/mjc/stage2/impl/H2ConnectionFactory.java
+++ task3-factory-pattern/src/main/java/com/mjc/stage2/impl/H2ConnectionFactory.java
@@ -2,34 +2,58 @@ package com.mjc.stage2.impl;
 
 import com.mjc.stage2.ConnectionFactory;
 import lombok.Value;
-import org.h2.jdbc.JdbcConnection;
 
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.ConnectionBuilder;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class H2ConnectionFactory implements ConnectionFactory {
-    public static Connection connection;
-    public static String driver;
-    public static String connectionURL;
-    public static String user;
-    public static String password;
+    private final static String JDBC_DRIVER;
+    private final static String DB_URL;
+    private final static String USER;
+    private final static String PASSWORD;
+
+    static {
+
+        Properties connectionProperties = new Properties();
+
+        try {
+            connectionProperties.load(
+                    H2ConnectionFactory.class
+                            .getClassLoader()
+                            .getResourceAsStream("h2database.properties")
+            );
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        JDBC_DRIVER = connectionProperties.getProperty("jdbc_driver");
+        DB_URL = connectionProperties.getProperty("db_url");
+        USER = connectionProperties.getProperty("user");
+        PASSWORD = connectionProperties.getProperty("password");
+
+    }
+
+    static {
+
+        try {
+            Class.forName(JDBC_DRIVER);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
     @Override
     public Connection createConnection() {
         try {
-            // Loading the driver
-            Class.forName(driver);
-            // Creating a connection
-            connection = DriverManager.getConnection(connectionURL, user, password);
-        } catch (ClassNotFoundException exception) {
-            System.out.println("Driver not found");
-        } catch (SQLException exception) {
-            System.out.println("Connection exception" + exception);
+            return DriverManager.getConnection(DB_URL, USER, PASSWORD);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
-
-        return connection;
-
     }
 
     // Write your code here!
