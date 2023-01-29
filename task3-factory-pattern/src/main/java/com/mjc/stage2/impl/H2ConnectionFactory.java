94a1496e84366d6295192b6cd03855b1a85a4340
EDyugaeva <ed2408@yandex.ru>
2023-01-29 18:46:21 +0500

    commit

--- task3-factory-pattern/src/main/java/com/mjc/stage2/impl/H2ConnectionFactory.java
+++ task3-factory-pattern/src/main/java/com/mjc/stage2/impl/H2ConnectionFactory.java
@@ -1,61 +1,33 @@
 package com.mjc.stage2.impl;
 
 import com.mjc.stage2.ConnectionFactory;
-import lombok.Value;
 
 import java.io.IOException;
 import java.sql.Connection;
-import java.sql.ConnectionBuilder;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
 
 public class H2ConnectionFactory implements ConnectionFactory {
-    private final static String JDBC_DRIVER;
-    private final static String DB_URL;
-    private final static String USER;
-    private final static String PASSWORD;
-
-    static {
-
-        Properties connectionProperties = new Properties();
-
-        try {
-            connectionProperties.load(
-                    H2ConnectionFactory.class
-                            .getClassLoader()
-                            .getResourceAsStream("h2database.properties")
-            );
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-        JDBC_DRIVER = connectionProperties.getProperty("jdbc_driver");
-        DB_URL = connectionProperties.getProperty("db_url");
-        USER = connectionProperties.getProperty("user");
-        PASSWORD = connectionProperties.getProperty("password");
-
-    }
-
-    static {
-
-        try {
-            Class.forName(JDBC_DRIVER);
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        }
-
-    }
-
-    @Override
     public Connection createConnection() {
+        Connection conn = null;
         try {
-            return DriverManager.getConnection(DB_URL, USER, PASSWORD);
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-    }
+            Properties appProps = new Properties();
+            appProps.load(H2ConnectionFactory.class.getClassLoader().getResourceAsStream("h2database.properties"));
 
+            String driver = appProps.getProperty("jdbc_driver");
+            String url = appProps.getProperty("db_url");
+            String user = appProps.getProperty("user");
+            String password = appProps.getProperty("password");
+
+            Class.forName(driver);
+            conn = DriverManager.getConnection(url, user, password);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return conn;
+    }
     // Write your code here!
 }
 
